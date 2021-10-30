99aa62a1a563a41d19e2fef0f9a10746935991ce
TLehtila <72321149+TLehtila@users.noreply.github.com>
2021-10-30 22:31:43 +0100

    For problem solving.

---
+++ wwwroot/js/website.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $('#page-content-wrapper').load('main.html');
+    $('#code').click(function () {
+        $('#page-content-wrapper').load('code.html');
+    })
+    $('#threed').click(function () {
+        $('#page-content-wrapper').load('threed.html');
+    })
+    $('#photo').click(function () {
+        $('#page-content-wrapper').load('photo.html');
+    })
+})
